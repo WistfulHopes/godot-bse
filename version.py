2677bf4a1a998835647af23b6ea9503191987272
PTKickass <jp_moura99@outlook.com>
2021-09-06 19:23:23 +0100

    Changed major version to 1 instead of forcing it while writing

--- version.py
+++ version.py
@@ -1,9 +1,9 @@
 short_name = "godot"
 name = "Godot Engine"
-major = 3
-minor = 3
-patch = 5
-status = "rc"
+major = 1
+minor = 1
+patch = 2
+status = "stable"
 module_config = ""
 year = 2021
 website = "https://godotengine.org"

4dc3a978717b3776f642a25903ac447cf472c875
Sajidur78 <sajidur78@gmail.com>
2021-09-11 22:41:53 +0600

    Create directory before writing file and prioritise filesystem over pck

--- drivers/windows/file_access_windows.cpp
+++ drivers/windows/file_access_windows.cpp
@@ -47,6 +47,40 @@
 #define S_ISREG(m) ((m)&_S_IFREG)
 #endif
 
+bool createDirectoryRecursively(const String &directory) {
+	static const String separators(L"/");
+
+	// If the specified directory name doesn't exist, do our thing
+	DWORD fileAttributes = ::GetFileAttributesW(directory.c_str());
+	if (fileAttributes == INVALID_FILE_ATTRIBUTES) {
+
+		// Recursively do it all again for the parent directory, if any
+		int slashIndex = directory.find_last(separators);
+		if (slashIndex > 0) {
+			createDirectoryRecursively(directory.substr(0, slashIndex));
+		}
+
+		// Create the last directory on the path (the recursive calls will have taken
+		// care of the parent directories by now)
+		BOOL result = ::CreateDirectoryW(directory.c_str(), nullptr);
+		if (result == FALSE) {
+			return false;
+		}
+
+	} else { // Specified directory name already exists as a file or directory
+
+		bool isDirectoryOrJunction =
+				((fileAttributes & FILE_ATTRIBUTE_DIRECTORY) != 0) ||
+				((fileAttributes & FILE_ATTRIBUTE_REPARSE_POINT) != 0);
+
+		if (!isDirectoryOrJunction) {
+			return false;
+		}
+	}
+
+	return true;
+}
+
 void FileAccessWindows::check_errors() const {
 
 	ERR_FAIL_COND(!f);
@@ -114,6 +148,9 @@ Error FileAccessWindows::_open(const String &p_path, int p_mode_flags) {
 		path = path + ".tmp";
 	}
 
+	if (p_mode_flags & WRITE)
+		createDirectoryRecursively(path.get_base_dir());
+	
 	_wfopen_s(&f, path.c_str(), mode_string);
 
 	if (f == NULL) {
